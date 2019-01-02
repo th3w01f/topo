4bd005b192347de6638e2cf0fd3f503ae10f28b9
th3w01f <opensystem.992@gmail.com>
2019-01-02 19:26:33 -0400

    script for topo and requeriments.txt
    first upload

---
+++ dorkerworker.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import os
+#vars
+usrAns = 's'
+
+def presentation():
+	print("!#[WorkerDorker]#!")
+	print("Generador de dorks para flojos")
+
+def opts():
+	print("""
+	1-[SQLInyectionDorks]
+	2-[DevicesAndCameras]
+	3-[LoginPages]
+	4-[UserAndPassword]
+	5-[Vulnerabilities]
+	6-[Interest Directories]
+	7-[Web Server Banners]
+	8-[vulnerable systems]
+	9-[network vulnerabilities]
+	10-[interest info]
+	11-[errors]
+	b1-[exploit-db]
+	""")
+	cont = 0
+	opc = input('opcion > ')
+	if(opc == '1'):
+		f = open("sqlinydork")
+		for line in f:
+			print('D'+str(cont))
+			print(line)
+			cont += 1
+			print('---')
+	if(opc == '2'):
+		f = open("devcams")
+		for line in f:
+			print('D'+str(cont))
+			print(line)
+			cont += 1
+			print('---')
+	if(opc == '3'):
+		f = open("loginpages")
+		for line in f:
+			print('D'+str(cont))
+			print(line)
+			cont += 1
+			print('---')
+	if(opc == '4'):
+		f = open("usrpsw")
+		for line in f:
+			print('D'+str(cont))
+			print(line)
+			cont += 1
+			print('---')
+	if(opc == '5'):
+		f = open("vulns")
+		for line in f:
+			print('D'+str(cont))
+			print(line)
+			cont += 1
+			print('---')
+	if(opc == '6'):
+		f = open("interestdir")
+		for line in f:
+			print('D'+str(cont))
+			print(line)
+			cont += 1
+			print('---')
+	if(opc == '7'):
+		f = open("webservbanners")
+		for line in f:
+			print('D'+str(cont))
+			print(line)
+			cont += 1
+			print('---')
+	if(opc == '8'):
+		f = open("sysvulns")
+		for line in f:
+			print('D'+str(cont))
+			print(line)
+			cont += 1
+			print('---')
+	if(opc == '9'):
+		f = open("netvulns")
+		for line in f:
+			print('D'+str(cont))
+			print(line)
+			cont += 1
+			print('---')
+	if(opc == '10'):
+		f = open("interestinfo")
+		for line in f:
+			print('D'+str(cont))
+			print(line)
+			cont += 1
+			print('---')
+	if(opc == '11'):
+		f = open("errors")
+		for line in f:
+			print('D'+str(cont))
+			print(line)
+			cont += 1
+			print('---')
+	if(opc == 'b1'):
+		os.system('w3m https://www.exploit-db.com/google-hacking-database/')
+
